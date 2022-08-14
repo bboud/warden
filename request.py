4d4edf94584b59f6478d6ed956ae4d8a2ac8dae6
Brandon Boudreaux <hf3209@wayne.edu>
2022-08-14 15:13:48 -0400

    Real thread safety

--- request.py
+++ request.py
@@ -2,47 +2,62 @@ from threading import Thread
 from gpiozero import PWMLED, Button, GPIOPinInUse
 from requests import get, Timeout
 from time import sleep
+from collections import deque
 import requests
+import queue
 
 class Ack(Thread):
-    def __init__(self):
+    def __init__(self, ackq):
         super().__init__()
         self.ack_led = PWMLED(21)
         self.ack_button = Button(20)
+        self.ackq = ackq
 
     def run(self):
         try:
             self.ack_led.pulse()
             self.ack_button.wait_for_press()
             self.ack_led.off()
-        except GPIOPinInUse:
+        except GPIOPinInUse as e:
+            print(e)
             print('GPIO ACK Pin in use!')
 
-        # Must delete or else pins will stay in use!
-        del self.ack_led
-        del self.ack_button
+        # Make sure these are no longer in use
+        self.ack_led.close()
+        self.ack_button.close()
+        
+        # Return to default state
+        self.ackq.append(False)
 
 class Request(Thread):
-    def __init__(self, same_info=[], location_info=[]):
+    def __init__(self, alert_controller_queue):
         super().__init__()
-        self.ack = Ack()
 
-        # Key: ID, Value: Alert Thread
-        self.alerts = {}
+        self.acq = alert_controller_queue
+        self.ackq = deque()
+        self.acked = False
 
         self.id_graveyard = []
+        self.threads = []
 
     def run(self):
         STATE_CODE = "AZ"
 
         while True:
             try:
+                self.acked = self.ackq.pop()
+            except:
+                print('Queue is empty')
+
+            try:
                 req = get(f'https://api.weather.gov/alerts/active?area={STATE_CODE}')
-            except requests.Timeout:
+            except requests.Timeout as e:
+                print(e)
                 print('API is timing out! Sleeping for 30 seconds.')
                 sleep(30)
                 continue
-            except requests.ConnectionError:
+            except requests.ConnectionError as e:
+                print(e)
                 print('Network connection error. Please check network connection. Sleeping for 30 seconds.')
                 sleep(30)
                 continue
@@ -50,7 +65,7 @@ class Request(Thread):
             if req.status_code != 200:
                 print(f'Request failed with status code {req.status_code}')
                 # Exit as API call is not valid and will never work.
-                exit()
+                break
 
             buffer = req.json()
 
@@ -62,20 +77,17 @@ class Request(Thread):
             for feature in buffer['features']:
                 if feature['id'] in self.id_graveyard: continue
 
-                print(feature['id'])
-
-                properties = feature['properties']
-
-                print(properties['event'])
-
                 self.id_graveyard.append(feature['id'])
 
+                print(feature['properties']['event'])
+
                 try:
-                    self.ack.start()
-                except RuntimeError:
-                    # If the current ack thread is dead, start a new one.
-                    if not self.ack.is_alive():
-                        self.ack = Ack()
-                        self.ack.start()
-                    sleep(10)
+                    if not self.acked:
+                        self.acked = True
+                        self.threads.append(Ack(self.ackq).start())
+                except RuntimeError as e:
+                    print(e)
                     continue
+            sleep(10)
+        for t in self.threads:
+            t.join()
